f2cf7007aa79b0887cd5674e678292a913c2c927
lucasfdcampos <_83_wm_tz_>
2019-11-19 22:06:37 -0300

    The stage is set, the Green Flag Drops.

--- fire.js
+++ fire.js
@@ -1,9 +1,19 @@
+const firePixelsArray = []
+const fireWidth = 10
+const fireHeight = 10
+
 function start() {
 
+    createFireDataStructure()
+    renderFire()
 }
 
 function createFireDataStructure() {
+    const numberOfPixels = fireWidth * fireHeight
 
+    for (let i = 0; i < numberOfPixels; i++) {
+        firePixelsArray[i] = 0
+    }
 }
 
 function calculateFirePropagation() {
@@ -11,7 +21,25 @@ function calculateFirePropagation() {
 }
 
 function renderFire() {
+    let html = '<table cellpadding=0 cellspacing=0>'
 
+    for (let row = 0; row < fireHeight; row++) {
+        html += '<tr>'
+
+        for (let column = 0; column < fireWidth; column++) {
+            const pixelIndex = column + (fireWidth * row)
+
+            html += '<td>'
+            html += pixelIndex
+            html += '</td>'
+        }
+
+        html += '</tr>'
+    }
+
+    html += '</table>'
+
+    document.querySelector('#fireCanvas').innerHTML = html
 }
 
 start()
